7818cd24e933bf43a887ac5593c353dfcbc1672d
BenBergCapital <ben512@hotmail.co.uk>
2013-08-11 20:43:23 +0100

    vochart

--- Webservicetest/src/com/ben/service/Bloomberg_scrape.java
+++ Webservicetest/src/com/ben/service/Bloomberg_scrape.java
@@ -17,7 +17,14 @@ public class Bloomberg_scrape {
 		}
 
 	}
-	
+	public String getIndex(String value) {
+		try {
+			return scrape_Future(Index.valueOf(value));
+		} catch (IOException e) {
+			return "--error--";
+		}
+
+	}
 	
 	private String scrape_value() throws IOException {
 		Document doc_curr = Jsoup.connect("Http://www.bloomberg.com/markets/").get();
@@ -36,6 +43,48 @@ public class Bloomberg_scrape {
 		}
 		return "0";
 	}
+	private enum Index {
+	    sap, nasdaq;
+	}
+private String scrape_Future(Index _Index) throws IOException
+{
 
-	
+	  Document doc_fut = Jsoup.connect("Http://www.bloomberg.com/markets/").get();
+		
+		 Element content_fut = doc_fut.getElementById("futures_data_table");
+		 Elements links_fut = content_fut.getElementsByClass("value");
+		 Elements links_fut_change = content_fut.getElementsByClass("percent_change");
+		 int l=0;
+		 String result = "";
+		 String result_chg = "";
+		 
+		 int _k=0;
+		 switch(_Index) 
+			 {
+			    case sap:
+			        _k=1;
+			        break;
+			    case nasdaq:
+			    	_k=2;
+			        break;	
+			}
+		 for (Element link : links_fut) 
+		 {
+			if (l==_k) 	result= link.text();
+			l++;
+		 }
+		 for (Element link : links_fut_change) 
+		 {
+			if (l==_k) 	result_chg= link.text();
+			l++;
+		 }
+		 
+		 
+		 
+		 return result+";"+result_chg;
+}
+
+
+
+
 }
